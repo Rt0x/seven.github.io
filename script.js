5481c181b36c520161991469669c3769090aae0a
Rt0x <144723959+Rt0x@users.noreply.github.com>
2023-11-15 20:35:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
-// Получаем ссылки на необходимые элементы
-const optionsSelect = document.getElementById('options');
+// Получение ссылок на элементы формы
 const quantityInput = document.getElementById('quantity');
-const serviceTypeInputs = document.querySelectorAll('input[name="serviceType"]');
-const propertyCheckbox = document.getElementById('property');
-const totalPriceSpan = document.getElementById('totalPrice');
+const serviceTypeInputs = document.getElementsByName('serviceType');
+const productOptionSelect = document.getElementById('productOption');
+const productPropertyCheckbox = document.getElementById('productProperty');
+const priceOutput = document.getElementById('price');
 
-// Определение стоимостей услуги
-const prices = {
-  type1: 200,
-  type2: 0
-};
-
-// Обработчик изменения опций, количества, типа услуги и свойства
-function calculateTotalPrice() {
-  const selectedOption = parseInt(optionsSelect.value);
+// Функция для пересчета цены
+function calculatePrice() {
+  // Получение выбранных значений
   const quantity = parseInt(quantityInput.value);
-  const selectedServiceType = document.querySelector('input[name="serviceType"]:checked').value;
-  let totalPrice = prices[selectedServiceType] * quantity;
-}
+  const serviceType = document.querySelector('input[name="serviceType"]:checked').value;
+  const productOption = productOptionSelect.value;
+  const productProperty = productPropertyCheckbox.checked;
 
+  // Выполнение расчетов
+  let basePrice = 0;
+  switch (serviceType) {
+    case 'basic':
+      basePrice = 100;
+      break;
+    case 'standard':
+      basePrice = 0;
+      break;
+  }
+
+  let optionPrice = 0;
+  switch (productOption) {
+    case 'option1':
+      optionPrice = 150;
+      break;
+    case 'option2':
+      optionPrice = 100;
+      break;
+    case 'option3':
+      optionPrice = 200;
+      break;
+      case 'option4':
+      optionPrice = 50;
+      break;
+    case 'option5':
+      optionPrice = 170;
+      break;
+    case 'option6':
+      optionPrice = 70;
+      break;
+      case 'option7':
+      optionPrice = 90;
+      break;
+    case 'option8':
+      optionPrice = 150;
+      break;
+   
+  }
+
+  let propertyPrice = productProperty ? 150 : 0;
+
+  const totalPrice = (basePrice + optionPrice + propertyPrice) * quantity;
+
+  // Обновление вывода цены
+  priceOutput.textContent = totalPrice;
+}
